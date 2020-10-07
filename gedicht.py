6a208e64516a4e30a881e6bb11b749dc3b4b260d
Matthew Willems <matthewwillems2003@outlook.com>
2020-10-07 12:14:09 +0200

    gedicht

---
+++ gedicht.py
@@ -0,0 +1,16 @@
+vers1 = "seconden die tikken"
+vers2 = "minuten die duren"
+vers3 = "de uren verstrijken"
+vers4 = "wat duren de dagen"
+
+vers5 = "de weken die glijden"
+vers6 = "de maanden die vloeien"
+vers7 = "de jaren die lijken"
+vers8 = "het leven te dragen"
+
+vers9 = "de haast is verdwenen"
+vers10 = "de snelheid vertraagt"
+vers11 = "voorbij gaan momenten"
+vers12 = "in stilte vertraagt"
+
+print(vers1,vers2,vers3,vers4,vers5,vers6,vers7,vers8,vers9,vers10,vers11,vers12)
