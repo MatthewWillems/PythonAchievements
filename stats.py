10e3e1ffa4eeb0fa0f798e460a0f59f607f06ab5
Matthew Willems <matthewwillems2003@outlook.com>
2020-10-18 20:20:43 +0200

    nieuw

---
+++ stats.py
@@ -0,0 +1,21 @@
+name = "Matthew"
+level = 17
+score = 2003
+offense = 99
+defense = 99
+speed = 70 
+stamina = 50
+strength = 40
+jumpforce = 50
+lives = 99
+
+print("Name: " + str(name))
+print("Level: " + str(level))
+print("Score: " + str(score))
+print("Offense: " + str(offense))
+print("Defense: " + str(defense))
+print("speed: " + str(speed))
+print("stamina: " + str(stamina))
+print("strength: " + str(strength))
+print("Jumpforce: " + str(jumpforce))
+print("Lives: " + str(lives))
