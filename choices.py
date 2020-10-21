d4a056bc08a1da955d39814de370a86de6420f55
Matthew Willems <matthewwillems2003@outlook.com>
2020-10-21 20:15:53 +0200

    choices

---
+++ choices.py
@@ -0,0 +1,44 @@
+print("ROOD of BLAUW")
+choice = input()
+if choice == 'ROOD':
+    print("ROOD")
+elif choice == 'BLAUW':
+    print("BLAUW")
+else:
+    print("GROEN")
+
+print("ZOMER of WINTER")
+choice = input()
+if choice == 'ZOMER':
+    print("ZOMER")
+elif choice == 'WINTER':
+    print("WINTER")
+else:
+    print("HERFST")
+
+print("HOOG of LAAG")
+choice = input()
+if choice == 'HOOG':
+    print("HOOG")
+elif choice == 'LAAG':
+    print("LAAG")
+else:
+    print("GEMIDDELD")
+
+print("WARM of KOUD")
+choice = input()
+if choice == 'WARM':
+    print("WARM")
+elif choice == 'KOUD':
+    print("KOUD")
+else:
+    print("LAUW")
+
+print("JA of NEE")
+choice = input()
+if choice == 'JA':
+    print("JA")
+elif choice == 'NEE':
+    print("NEE")
+else:
+    print("MISSCHIEN")
---
+++ operations.py
@@ -0,0 +1,12 @@
+a = 15000
+b = 12
+c = 100
+d = 99
+e = 50
+f = 400
+
+print((a*b)/c)
+print(c+d)
+print(d%4)
+print(f-e)
+while 
